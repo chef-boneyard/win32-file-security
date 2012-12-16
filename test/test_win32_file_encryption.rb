7715736db184999437a1812652ee3588b5e66c27
Daniel Berger <djberg96@gmail.com>
2012-12-16 08:48:46 -0700

    Encryption tests now skipped unless running with elevated security.

--- test/test_win32_file_encryption.rb
+++ test/test_win32_file_encryption.rb
@@ -7,43 +7,66 @@
 # Note: These tests may fail based on the security setup of your system.
 #############################################################################
 require 'test-unit'
+require 'win32/security'
 require 'win32/file/security'
+require 'socket'
 
 class TC_Win32_File_Encryption < Test::Unit::TestCase
   def self.startup
     Dir.chdir(File.dirname(File.expand_path(File.basename(__FILE__))))
     @@file = File.join(Dir.pwd, 'encryption_test.txt')
     File.open(@@file, 'w'){ |fh| fh.puts "This is an encryption test." }
+    @@host = Socket.gethostname
   end
 
   def setup
     @msg = '=> Ignore. May not work due to security setup of your system.'
+    @elevated = Win32::Security.elevated_security?
   end
 
   test "encrypt method basic functionality" do
+    omit_unless(@elevated)
     assert_respond_to(File, :encrypt)
     assert_nothing_raised(@msg){ File.encrypt(@@file) }
   end
 
-  test "encrypt accepts a single argument only" do
+  test "encrypt accepts an optional host name" do
+    omit_unless(@elevated)
+    assert_nothing_raised(@msg){ File.encrypt(@@file, @@host) }
+  end
+
+  test "encrypt accepts at least one argument" do
+    omit_unless(@elevated)
     assert_raise(ArgumentError){ File.encrypt }
   end
 
   test "encrypt requires a string argument" do
-    assert_raise(TypeError){ File.encrypt(1) }
+    omit_unless(@elevated)
+    assert_raise([TypeError, NoMethodError]){ File.encrypt(1) }
   end
 
   test "decrypt method basic functionality" do
+    omit_unless(@elevated)
     assert_respond_to(File, :decrypt)
     assert_nothing_raised(@msg){ File.decrypt(@@file) }
   end
 
   test "decrypt accepts a single argument only" do
+    omit_unless(@elevated)
     assert_raise(ArgumentError){ File.decrypt }
   end
 
   test "decrypt requires a string argument" do
-    assert_raise(TypeError){ File.decrypt(1) }
+    omit_unless(@elevated)
+    assert_raise([TypeError, NoMethodError]){ File.decrypt(1) }
+  end
+
+  test "encryptable? basic functionality" do
+    assert_respond_to(File, :encryptable?)
+  end
+
+  test "encryptable? returns a boolean value" do
+    assert_boolean(File.encryptable?("C:\\"))
   end
 
   def teardown
@@ -53,5 +76,6 @@ class TC_Win32_File_Encryption < Test::Unit::TestCase
   def self.shutdown
     File.delete(@@file) if File.exists?(@@file)
     @@file = nil
+    @@host = nil
   end
 end
