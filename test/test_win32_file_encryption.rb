a769584fa8e93569f6f0c529a00cc7008c387ead
Daniel Berger <djberg96@gmail.com>
2012-12-19 07:54:04 -0700

    Some test cleanup and additions.

--- test/test_win32_file_encryption.rb
+++ test/test_win32_file_encryption.rb
@@ -22,6 +22,7 @@ class TC_Win32_File_Encryption < Test::Unit::TestCase
   def setup
     @msg = '=> Ignore. May not work due to security setup of your system.'
     @elevated = Win32::Security.elevated_security?
+    @statuses = ['encrypted', 'encryptable', 'unknown']
   end
 
   test "encrypt method basic functionality" do
@@ -30,19 +31,15 @@ class TC_Win32_File_Encryption < Test::Unit::TestCase
     assert_nothing_raised(@msg){ File.encrypt(@@file) }
   end
 
-  test "encrypt accepts an optional host name" do
-    omit_unless(@elevated)
-    assert_nothing_raised(@msg){ File.encrypt(@@file, @@host) }
-  end
-
-  test "encrypt accepts at least one argument" do
+  test "encrypt requires one argument" do
     omit_unless(@elevated)
     assert_raise(ArgumentError){ File.encrypt }
+    assert_raise(ArgumentError){ File.encrypt(@@file, @@file) }
   end
 
   test "encrypt requires a string argument" do
     omit_unless(@elevated)
-    assert_raise([TypeError, NoMethodError]){ File.encrypt(1) }
+    assert_raise(TypeError, NoMethodError){ File.encrypt(1) }
   end
 
   test "decrypt method basic functionality" do
@@ -58,7 +55,7 @@ class TC_Win32_File_Encryption < Test::Unit::TestCase
 
   test "decrypt requires a string argument" do
     omit_unless(@elevated)
-    assert_raise([TypeError, NoMethodError]){ File.decrypt(1) }
+    assert_raise(TypeError, NoMethodError){ File.decrypt(1) }
   end
 
   test "encryptable? basic functionality" do
@@ -69,8 +66,20 @@ class TC_Win32_File_Encryption < Test::Unit::TestCase
     assert_boolean(File.encryptable?("C:\\"))
   end
 
+  test "encryption_status basic functionality" do
+    assert_respond_to(File, :encryption_status)
+  end
+
+  test "encryption_status returns the expected result" do
+    status = File.encryption_status(@@file)
+    assert_kind_of(String, status)
+    assert_true(@statuses.include?(status))
+  end
+
   def teardown
     @msg = nil
+    @statuses = nil
+    @elevated = nil
   end
 
   def self.shutdown
