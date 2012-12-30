6247725064ebc66a62042265c4d0eafdbb47513a
Daniel Berger <djberg96@gmail.com>
2012-12-30 13:02:48 -0700

    Added File.chown tests.

--- test/test_win32_file_security_ownership.rb
+++ test/test_win32_file_security_ownership.rb
@@ -5,6 +5,7 @@
 #############################################################################
 require 'etc'
 require 'socket'
+require 'sys/admin'
 require 'test-unit'
 require 'win32/security'
 require 'win32/file/security'
@@ -14,12 +15,18 @@ class TC_Win32_File_Security_Ownership < Test::Unit::TestCase
     Dir.chdir(File.dirname(File.expand_path(File.basename(__FILE__))))
     @@file = File.join(Dir.pwd, 'ownership_test.txt')
     File.open(@@file, 'w'){ |fh| fh.puts "This is an ownership test." }
+
+    @@host  = Socket.gethostname
+    @@temp  = "Temp"
+    @@login = Etc.getlogin
+
+    if Win32::Security.elevated_security?
+      Sys::Admin.add_user(:name => @@temp, :description => "Delete Me")
+    end
   end
 
   def setup
     @elevated = Win32::Security.elevated_security?
-    @login    = Etc.getlogin
-    @host     = Socket.gethostname
   end
 
   test "owned? method basic functionality" do
@@ -29,7 +36,11 @@ class TC_Win32_File_Security_Ownership < Test::Unit::TestCase
   end
 
   test "owned? method returns expected result" do
-    assert_true(File.owned?(@@file))
+    if Win32::Security.elevated_security?
+      assert_false(File.owned?(@@file))
+    else
+      assert_true(File.owned?(@@file))
+    end
     assert_false(File.owned?("C:\\Windows\\regedit.exe"))
   end
 
@@ -45,7 +56,11 @@ class TC_Win32_File_Security_Ownership < Test::Unit::TestCase
   end
 
   test "owner method returns the expected value" do
-    expected = @host << "\\" << @login
+    if Win32::Security.elevated_security?
+      expected = "BUILTIN\\Administrators"
+    else
+      expected = @@host + "\\" + @@login
+    end
     assert_equal(expected, File.owner(@@file))
   end
 
@@ -54,14 +69,30 @@ class TC_Win32_File_Security_Ownership < Test::Unit::TestCase
     assert_raise(ArgumentError){ File.owner(@@file, @@file) }
   end
 
+  test "chown method basic functionality" do
+    assert_respond_to(File, :chown)
+  end
+
+  test "chown works as expected" do
+    omit_unless(@elevated)
+    original_owner = File.owner(@@file)
+    expected_owner = @@host + "\\" + @@temp
+
+    assert_nothing_raised{ File.chown(@@temp, nil, @@file) }
+    assert_equal(expected_owner, File.owner(@@file))
+    assert_nothing_raised{ File.chown(original_owner, nil, @@file) }
+    assert_equal(original_owner, File.owner(@@file))
+  end
+
   def teardown
     @elevated = nil
-    @login = nil
-    @host = nil
   end
 
   def self.shutdown
+    Sys::Admin.delete_user(@@temp) if Win32::Security.elevated_security?
     File.delete(@@file) if File.exists?(@@file)
     @@file = nil
+    @@login = nil
+    @@host = nil
   end
 end
