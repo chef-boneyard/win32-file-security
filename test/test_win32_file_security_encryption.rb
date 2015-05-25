3062fab6e50414e11f2757d540241a3cbbc689eb
Daniel Berger <djberg96@gmail.com>
2015-05-25 10:47:15 -0600

    Fixed deprecation warning.

--- test/test_win32_file_security_encryption.rb
+++ test/test_win32_file_security_encryption.rb
@@ -83,7 +83,7 @@ class TC_Win32_File_Security_Encryption < Test::Unit::TestCase
   end
 
   def self.shutdown
-    File.delete(@@file) if File.exists?(@@file)
+    File.delete(@@file) if File.exist?(@@file)
     @@file = nil
     @@host = nil
   end
