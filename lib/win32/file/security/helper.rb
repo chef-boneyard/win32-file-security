a703361bc7341a44ff17175f5e30fd6c547a535d
Daniel Berger <dberger@redhat.com>
2015-05-28 08:24:14 -0600

    Avoid potential redefinition, check for current encoding before modification.

--- lib/win32/file/security/helper.rb
+++ lib/win32/file/security/helper.rb
@@ -1,13 +1,23 @@
 class String
-  # Convenience method for converting strings to UTF-16LE for wide character
-  # functions that require it.
-  def wincode
-    (self.tr(File::SEPARATOR, File::ALT_SEPARATOR) + 0.chr).encode('UTF-16LE')
+  unless defined? :wincode
+    # Convenience method for converting strings to UTF-16LE for wide character
+    # functions that require it.
+    def wincode
+      unless encoding == Encoding::UTF_16LE
+        self.tr(File::SEPARATOR, File::ALT_SEPARATOR) + 0.chr).encode('UTF-16LE')
+      end
+    end
   end
 
-  # Read a wide character string up until the first double null, and delete
-  # any remaining null characters.
-  def wstrip
-    self.force_encoding('UTF-16LE').encode('UTF-8',:invalid=>:replace,:undef=>:replace).split("\x00")[0].encode(Encoding.default_external)
+  unless defined? :wstrip
+    # Read a wide character string up until the first double null, and delete
+    # any remaining null characters.
+    def wstrip
+      unless encoding == Encoding::UTF_16LE
+        self.force_encoding('UTF-16LE')
+      end
+
+      self.encode('UTF-8',:invalid=>:replace,:undef=>:replace).split("\x00")[0].encode(Encoding.default_external)
+    end
   end
 end
