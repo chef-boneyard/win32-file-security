d9359d0ad2723ff9f92fb68aceb29daca7811eea
Daniel Berger <djberg96@gmail.com>
2016-01-30 19:45:37 -0700

    Version bump.

--- lib/win32/file/security.rb
+++ lib/win32/file/security.rb
@@ -12,7 +12,7 @@ class File
   extend Windows::File::Functions
 
   # The version of the win32-file library
-  WIN32_FILE_SECURITY_VERSION = '1.0.7'
+  WIN32_FILE_SECURITY_VERSION = '1.0.8'
 
   class << self
     remove_method(:chown)
